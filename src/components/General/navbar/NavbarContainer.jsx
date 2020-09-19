5bda82474da0efe9e9b4d6eadbcad790e1e7a9c5
Mpatel296 <mayhay96@gmail.com>
2020-09-20 01:52:04 +0200

    narbar items added and styling with hover

--- src/components/General/navbar/NavbarContainer.jsx
+++ src/components/General/navbar/NavbarContainer.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import NavItem from './NavItem';
 
 const NavbarContainer = () => {
     return(
         <div className = "navbar-container">
-
+            <NavItem navItem = 'Sport'/>
+            <NavItem navItem = 'Societies' />
+            <NavItem navItem = 'Education' />
+            <NavItem navItem = 'Guide to Olomouc' />
+            <NavItem navItem = 'Events' />
+            <NavItem navItem = 'Your Union' />
+            <NavItem navItem = 'Contact Us' />
         </div>
     )
 };
