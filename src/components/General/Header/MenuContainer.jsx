e61c5d6a594f60430f36746f378bf950a3eacd20
Mpatel296 <mayhay96@gmail.com>
2020-09-19 22:54:47 +0200

    styling homepage

---
+++ src/components/General/Header/MenuContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MenuContainer = () => {
+    return(
+        <div className= 'menu-container'>
+            <i class="fas fa-bars menu-icon"></i>
+        </div>
+    )
+};
+
+export default MenuContainer;
