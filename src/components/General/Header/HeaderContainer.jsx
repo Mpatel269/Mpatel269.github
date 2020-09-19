cc7ccc3971f28462e66d7174db8a736abea0d4a5
Mpatel296 <mayhay96@gmail.com>
2020-09-19 21:59:51 +0200

    logo addition

--- src/components/General/Header/HeaderContainer.jsx
+++ src/components/General/Header/HeaderContainer.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import LogoContainer from '../LogoContainer';
+import SocialsContainer from './SocialsContainer';
 
 const HeaderContainer = () => {
     return(
         <div className = "header-container">
-
+            <LogoContainer />
+            <SocialsContainer />
         </div>
     )
 };
