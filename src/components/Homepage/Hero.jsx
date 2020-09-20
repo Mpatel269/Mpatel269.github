2c5e0478d843647b4dca6db757f9206407422ca5
Mpatel296 <mayhay96@gmail.com>
2020-09-20 21:08:53 +0200

    navbar styling

--- src/components/Homepage/Hero.jsx
+++ src/components/Homepage/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return(
         <div className = "hero-image-container">
-
+            <p className = 'welcome-text'>Welcome to the new<br/> PEPA website!</p>
         </div>
     )
 }
