1ac9edab7a209e72018cd890f42f62c2f5b8d0a7
Mpatel296 <mayhay96@gmail.com>
2020-09-27 02:07:56 +0200

    second nav bar

--- src/components/General/navbar/NavbarContainer.jsx
+++ src/components/General/navbar/NavbarContainer.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import NavItem from './NavItem';
 import {useSelector} from 'react-redux';
+import UnionDropdown from './dropdownMenu/UnionDropdown';
 
 const NavbarContainer = (props) => {
     const showMenu = useSelector((state) => state.menuActive);
 
     return(
         <div className = {showMenu ? 'navbar-container-active' :'navbar-container-inactive'}>
-            <NavItem navItem = 'Your Union' />
+            <NavItem navItem = 'Your Union' >
+                <UnionDropdown />
+            </NavItem>
+
             <NavItem navItem = 'Sports'/>
             <NavItem navItem = 'Societies' />
             <NavItem navItem = 'Education' />
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import Footercontainer from '../components/General/footer/FooterContainer';
 import HeaderContainer from '../components/General/Header/HeaderContainer';
+import ContactDropdown from '../components/General/navbar/dropdownMenu/ContactDropdown';
+import EducationDropdown from '../components/General/navbar/dropdownMenu/EducationDropdown';
+import EventsDropdown from '../components/General/navbar/dropdownMenu/EventsDropdown';
+import GuideDropdown from '../components/General/navbar/dropdownMenu/GuideDropdown';
+import SocietiesDropdown from '../components/General/navbar/dropdownMenu/SocietiesDropdown';
 import SportsDropdown from '../components/General/navbar/dropdownMenu/SportsDropdown';
+import SupportDropdown from '../components/General/navbar/dropdownMenu/SupportDropdown';
+import UnionDropdown from '../components/General/navbar/dropdownMenu/UnionDropdown';
 import NavbarContainer from '../components/General/navbar/NavbarContainer';
 import Hero from '../components/Homepage/Hero';
 import HomepageBody from '../components/Homepage/HomepageBody';
@@ -12,7 +19,6 @@ const Homepage = () => {
         <div>
             <HeaderContainer />
             <NavbarContainer />
-                <SportsDropdown />
             <Hero />
             <HomepageBody />
             <Footercontainer />
