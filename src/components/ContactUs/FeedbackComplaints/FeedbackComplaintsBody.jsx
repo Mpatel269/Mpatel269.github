8283a91299875e62ffe01ba0cbb45227462c4c7c
Mpatel296 <mayhay96@gmail.com>
2021-12-07 10:25:15 +0100

    added feedback form

--- src/components/ContactUs/FeedbackComplaints/FeedbackComplaintsBody.jsx
+++ src/components/ContactUs/FeedbackComplaints/FeedbackComplaintsBody.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const FeedbackComplaintsBody = () => {
     return(
         <div className="feedback-complaints-body">
-            <iframe className="feedback-form" src="https://docs.google.com/forms/d/e/1FAIpQLSegO-c7t35mMbLaxR5C9OF15eWccKZrgxigJcl8uBDqV1qRGQ/viewform?embedded=true" position= "center" width="800" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+            <iframe title="feedbackForm" className="feedback-form" src="https://docs.google.com/forms/d/e/1FAIpQLSegO-c7t35mMbLaxR5C9OF15eWccKZrgxigJcl8uBDqV1qRGQ/viewform?embedded=true" position= "center" width="800" height="800" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
     )
 };
