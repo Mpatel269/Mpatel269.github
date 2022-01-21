ca9de3f401e04c634d05f806776ec3f7742a5749
Mpatel269 <66799033+Mpatel269@users.noreply.github.com>
2022-01-21 02:17:42 +0000

    deleted content from food body from previous test

--- src/pages/GuideToOlomouc/FoodPg.jsx
+++ src/pages/GuideToOlomouc/FoodPg.jsx
@@ -3,14 +3,12 @@ import Footercontainer from '../../components/General/footer/FooterContainer';
 import HeaderContainer from '../../components/General/Header/HeaderContainer';
 import NavbarContainer from '../../components/General/navbar/NavbarContainer';
 import FoodBody from '../../components/GuideToOlomouc/Food/FoodBody';
-import HeroSlider from '../../components/Homepage/HeroSlider';
 
 const FoodPg = () => {
     return(
         <div>
             <HeaderContainer/>
             <NavbarContainer/>
-            <HeroSlider/>
             <FoodBody/>
             <Footercontainer/>
         </div>
