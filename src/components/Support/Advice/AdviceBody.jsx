2d31dff0535db0238eb6558ae30284725ef46679
Mpatel269 <66799033+Mpatel269@users.noreply.github.com>
2022-02-05 12:53:33 +0100

    support page links

--- src/components/Support/Advice/AdviceBody.jsx
+++ src/components/Support/Advice/AdviceBody.jsx
@@ -24,13 +24,13 @@ const AdviceBody = () => {
                         </div>
                         <div class="flex flex-row justify-evenly">
                             <div class="p-4 m-6 rounded-xl border-2 hover:bg-gray-800 transform transition duration-200 hover:scale-105 hover:border-opacity-0">
-                                <i class="fab fa-facebook-messenger fa-4x"></i>
+                                <a href="https://facebook.com/UPOLPEPA" rel='noreferrer' target='_blank'><i class="fab fa-facebook-messenger fa-4x"></i></a>
                             </div>
                             <div class="p-4 m-6 rounded-xl border-2 hover:bg-gray-800 transform transition duration-200 hover:scale-105 hover:border-opacity-0">
                                 <i class="fas fa-envelope fa-4x"></i>
                             </div>
                             <div class="p-4 m-6 rounded-xl border-2 hover:bg-gray-800 transform transition duration-200 hover:scale-105 hover:border-opacity-0">
-                                <i class="fab fa-instagram fa-4x"></i>
+                                <a href="https://www.instagram.com/pepa.up" rel='noreferrer' target='_blank'><i class="fab fa-instagram fa-4x"></i></a>
                             </div>
                         </div>
                     </div>
