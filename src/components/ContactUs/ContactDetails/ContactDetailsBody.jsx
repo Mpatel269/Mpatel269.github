09188e89c24d36d4c382d03832d3e3757e26fb13
Mpatel269 <66799033+Mpatel269@users.noreply.github.com>
2022-03-04 17:25:34 +0100

    health layout

--- src/components/ContactUs/ContactDetails/ContactDetailsBody.jsx
+++ src/components/ContactUs/ContactDetails/ContactDetailsBody.jsx
@@ -2,8 +2,42 @@ import React from 'react';
 
 const ContactDetailsBody = () => {
     return(
-        <div>
-
+        <div class="">
+            <div class="flex flex-col">
+                <div class="flex items-center justify-center w-screen py-16 bg-white">
+                    <div class="flex flex-col h-10/12 w-10/12 bg-gray-900 text-white rounded-lg">
+                        <div class="text-center">
+                            <p>Reach out to us on any of these platforms</p>
+                        </div>
+                        <div class="">
+                            <div class="text-center p-6">
+                                <a href="">
+                                    <i class="fab fa-facebook-messenger fa-4x"></i>
+                                </a>
+                                <p>Facebook</p>
+                            </div>
+                            <div class="text-center p-6">
+                                <a href="">
+                                    <i class="fab fa-instagram fa-4x"></i>
+                                </a>
+                                <p>Instagram</p>
+                            </div>
+                            <div class="text-center p-6">
+                                <a href="">
+                                    <i class="fab fa-linkedin fa-4x"></i>
+                                </a>
+                                <p>LinkedIn</p>
+                            </div>
+                            <div class="text-center p-6">
+                                <a href="">
+                                    <i class="fas fa-envelope fa-4x"></i>
+                                </a>
+                                <p>Email</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
--- src/components/GuideToOlomouc/Health/HealthBody.jsx
+++ src/components/GuideToOlomouc/Health/HealthBody.jsx
@@ -3,7 +3,40 @@ import React from 'react';
 const HealthBody = () => {
     return(
         <div>
+            <div>
+                {/* Information toggle*/}
+                <div>
+                    <div>
+                        <ul>
+                            <li>
+                                <a href="#link1">
 
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#link2">
+
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                {/* Display */}
+                <div>
+                    <div>
+                        <div class="" id="link1">
+                            <div>
+
+                            </div>
+                        </div>
+                        <div class="" id="link2">
+                            <div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
