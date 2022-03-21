dec0fab153e7183bf0d6313e3ac54f0714b38a3b
Mpatel269 <66799033+Mpatel269@users.noreply.github.com>
2022-03-21 11:07:04 +0100

    competition body layout and information

--- src/components/Sports/Competitions/CompetitionsBody.jsx
+++ src/components/Sports/Competitions/CompetitionsBody.jsx
@@ -3,14 +3,40 @@ import React from 'react';
 const CompetitionsBody = () => {
     return(
         <div>
-            <h1>Competitions</h1>
-                <p>Every year, PEPA strives to organise interfaculty and intrafaculty sporting competitions and tournaments.</p>
-
-            <h3>Past Events:</h3>
-            <p>Include gallery</p>
-
-            <h3>Interested in collaborating with us?</h3>
-                <p>PEPA welcomes any opportunity to collaborate on sporting events with other student organisations. Feel free to get in touch with us at pepa.palacky@gmail.com</p>
+            <div class="py-8 bg-pepa-blue flex items-center justify-center ">
+                <div class="h-5/6 w-10/12 bg-gray-200 rounded-lg">
+                    <div class="h-1/6 p-8 bg-gray-50 rounded-t-lg flex flex-row items-center justify-center text-2xl font-semibold">
+                        <div class="px-4">
+                            <i class="fas fa-trophy"></i>
+                        </div>
+                        <p>Competitions and Tournaments</p>
+                        <div class="px-4">
+                            <i class="fas fa-trophy"></i>
+                        </div>
+                    </div>
+                    <div class="h-4/6 p-8 text-center">
+                        <p class="py-4">Every year PEPA strives to organise and participate in tournaments both intra and inter faculty.</p>
+                        <p class="py-4 font-semibold">Interested in collaborating with us?</p>
+                        <p class="py-4">We would welcome any opportunity to collaborate on sporting events with other student organisations. Get in touch with us via any of our social media outlets or by email:</p>
+                        <p class="py-4">Pepa.palacky@gmail.com</p>
+                    </div>
+                    <div class="rounded-b-lg text-center font-semibold bg-gray-50">
+                        <p class="pt-4 text-xl">Tournament Gallery</p>
+                        <br />
+                        <div class="py-8 flex flex-row space justify-evenly items">
+                            <div class="h-5/6 w-3/12">
+                                <img src="https://images.unsplash.com/photo-1578269174936-2709b6aeb913?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dG91cm5hbWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                            </div>
+                            <div class="h-5/6 w-3/12">
+                                <img src="https://images.unsplash.com/photo-1578269174936-2709b6aeb913?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dG91cm5hbWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                            </div>
+                            <div class="h-5/6 w-3/12">
+                                <img src="https://images.unsplash.com/photo-1578269174936-2709b6aeb913?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dG91cm5hbWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
